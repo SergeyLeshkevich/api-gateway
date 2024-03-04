50f537fdf2764abf27dedbd56f13609f2088bbfd
Sergey Leshkevich <s.leshkevichemail@gmail.com>
2024-03-04 15:38:29 +0300

    init

---
+++ src/test/java/ru/clevertec/gateway/ApiAtewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.clevertec.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAtewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
